88a5fdd9d286a0a039f67caf246e09398d65d94f
kancharlasaisruthi <saikancharla08@gmail.com>
2026-06-25 11:14:21 +0530

    llms connected

---
+++ src/langgraphagenticai/LLMS/groqllm.py
@@ -0,0 +1,22 @@
+import os
+## reading groq api key user submits in frontend
+
+import streamlit as st
+from langhchain_groq import ChatGroq
+
+class GroqLLM:
+    def __init__(self,user_controls_input):
+        self.user_controls_input=user_controls_input
+
+    def get_llm_model(self):
+        try:
+            groq_api_key=self.user_controls_input['GROQ_API_KEY']
+            selected_groq_model=self.user_controls_input['selected_groq_model']
+            if groq_api_key=='' and os.environ['GROQ_API_KEY']=='':
+                st.error('please enter the Groq API KEY')
+
+            llm=ChatGroq(api_key=groq_api_key,model=selected_groq_model)
+        
+        except Exception as e:
+            raise ValueError(f'Error Occured with exception:{e}')
+        return llm
